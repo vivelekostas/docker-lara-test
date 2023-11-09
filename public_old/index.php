a7c8174480825c1b7a1b8e5a957bb738c9ac658b
kostas <vivelakostas1096@gmail.com>
2023-11-09 15:57:48 +0300

    docker v.4 Laravel

---
+++ public_old/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Дока</title>
+    <link rel="stylesheet" href="/assets/styles/main.css">
+  </head>
+  <body>
+    <h2>Salut PHP!! Common ca va?</h2>
+    <br>
+    <?php echo 'ololo!!' ?>
+  </body>
+</html>
